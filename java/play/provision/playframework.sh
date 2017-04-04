9a2cccb3acb774d99efff48359a319e8e410ae35
高良陽平 <b195.clo5566@gmail.com>
2017-04-05 00:33:52 +0900

    add application install shell.

---
+++ java/play/provision/playframework.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget http://downloads.typesafe.com/typesafe-activator/1.3.12/typesafe-activator-1.3.12-minimal.zip
+unzip typesafe-activator-1.3.12-minimal.zip
+sudo rm -rf typesafe-activator-1.3.12-minimal.zip
+sudo mv activator-1.3.12-minimal /opt/activator
+sudo ln -sf /opt/activator/bin/activator /usr/local/bin/activator
