a4869ab60282b22e56ea2d9050426deec194d522
yohei-takara <yohei.takara@cacco.co.jp>
2017-06-07 12:25:06 +0900

    作成するuserが存在しない場合のみ作成するように変更

--- java/play/activator/provision/useradd.sh
+++ java/play/activator/provision/useradd.sh
@@ -5,5 +5,18 @@ USER="vagrant"
 # password "vagrant" を SHA-512 でハッシュ化
 PASSWORD=$(perl -e 'print crypt("vagrant", "\$6\$");')
 
-sudo useradd -p ${PASSWORD} -m ${USER}
-sudo usermod -G sudo ${USER}
+create_user(){
+
+  echo "${USER} is created."
+  # Create User
+  sudo useradd -p ${PASSWORD} -m ${USER}
+  # grant authrity
+  sudo usermod -G sudo ${USER}
+  exit 0
+}
+
+trap "create_user" ERR
+# check exist user
+getent passwd ${USER}
+
+echo "${USER} is already created."
